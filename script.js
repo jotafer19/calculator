6e5ddf95c170b48a7e36c56e565232b7c3d1970a
jotafer19 <115662522+jotafer19@users.noreply.github.com>
2023-08-24 22:48:17 +0200

    Add possibility to string together several operations

--- script.js
+++ script.js
@@ -14,43 +14,80 @@ function divide(a, b) {
     return Number(a) / Number(b);
 }
 
-let leftNumber;
+let leftNumber = "";
 let operator;
 let operatorUsed = false;
-let rightNumber;
+let rightNumber = "";
+let resultGot = false;
+let result;
+
+const buttonReset = document.querySelector("#reset");
+const buttonDelete = document.querySelector("#delete");
 const buttonNumber = document.querySelectorAll(".button-number");
 const buttonOperator = document.querySelectorAll(".button-operator");
 const buttonEqual = document.querySelector("#button-equal");
+const buttonDot = document.querySelector("#button-dot");
 const screenMainDisplay = document.querySelector("#main-display");
 const screenSubDisplay = document.querySelector("#sub-display");
+
+function resetCalculator() {
+    screenMainDisplay.textContent = 0;
+    screenSubDisplay.textContent = "";
+    leftNumber = "";
+    rightNumber = "";
+    operator = "";
+    operatorUsed = false;
+    resultGot = false;
+}
+
+function deleteDigit() {
+    console.log(leftNumber, operator, rightNumber)
+    let stringToRemove = leftNumber + operator + rightNumber;
+    console.log(stringToRemove);
+    let removedString = stringToRemove.slice(0, -1);
+    console.log(removedString);
+}
+
+buttonReset.addEventListener("click", resetCalculator);
+
+buttonDelete.addEventListener("click", deleteDigit);
 
 buttonNumber.forEach(button => {
     button.addEventListener("click", e => {
         if (!operatorUsed) {
-            (leftNumber === undefined) ? leftNumber = e.target.textContent : leftNumber += e.target.textContent;
+            if (resultGot === true) {
+                leftNumber = "";
+                resultGot = false; 
+            }
+            (leftNumber === "") ? leftNumber = e.target.textContent : leftNumber += e.target.textContent;
             screenMainDisplay.textContent = leftNumber;
         } else {
-            (rightNumber === undefined) ? rightNumber = e.target.textContent : rightNumber += e.target.textContent;
+            (rightNumber === "") ? rightNumber = e.target.textContent : rightNumber += e.target.textContent;
             screenMainDisplay.textContent = rightNumber;
         }
-        console.log(typeof(leftNumber));
-        console.log("Right number: ", rightNumber)
     })
 })
 
 buttonOperator.forEach(button => {
     button.addEventListener("click", e => {
-        if (leftNumber === undefined) leftNumber = 0;
-        operator = e.target.textContent;
-        console.log(operator);
-        screenSubDisplay.textContent = `${screenMainDisplay.textContent} ${operator} `;
-        operatorUsed = true;
+        if (leftNumber === "") leftNumber = 0;
+        if (operatorUsed) {
+            result = doOperation(rightNumber, operator);
+            resultGot = true;
+            leftNumber = result;
+            rightNumber = "";
+            operator = e.target.textContent;
+            screenSubDisplay.textContent = `${result} ${operator}`
+        } else {
+            operator = e.target.textContent;
+            screenSubDisplay.textContent = `${screenMainDisplay.textContent} ${operator} `;
+            operatorUsed = true;
+        }
     })
 })
 
-function getOperation(symbol) {
-    console.log("hey")
-    switch (symbol) {
+function getOperation(operator) {
+    switch (operator) {
         case "+":
             return add(leftNumber, rightNumber);
         case "-":
@@ -62,17 +99,22 @@ function getOperation(symbol) {
     }
 }
 
-
-buttonEqual.addEventListener("click", () => {
+function doOperation(rightNumber, operator) {
     if (rightNumber == 0 && operator === "÷") {
         alert("You can not divide by 0!");
-        rightNumber = undefined;
-    } else if (rightNumber !== undefined) {
-        let result = getOperation(operator, rightNumber);
-        screenSubDisplay.textContent = `${screenSubDisplay.textContent} ${rightNumber} =`
-        screenMainDisplay.textContent = result;
-        leftNumber = result;
-        rightNumber = undefined;
-        operatorUsed = false;
+        rightNumber = "";
+    } else if (rightNumber !== "") {
+        let operationCheck = getOperation(operator);
+        screenSubDisplay.textContent = `${leftNumber} ${operator} ${rightNumber} =`
+        screenMainDisplay.textContent = operationCheck;
+        return operationCheck;
     }
+}
+
+buttonEqual.addEventListener("click", () => {
+    result = doOperation(rightNumber, operator);
+    resultGot = true;
+    operatorUsed = false;
+    leftNumber = result;
+    rightNumber = "";
 })
